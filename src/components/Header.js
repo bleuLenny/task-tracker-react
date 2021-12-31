4cf28168cdc555a90257b3df38c4c8527e03af39
Alexander Amog <alex.amog@hotmail.com>
2021-12-31 13:28:38 -0800

    Added color to button

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text={showAdd ? "Close" : "Add"} onClick={onAdd} />
+      <Button color={showAdd ? 'black' : 'green'} text={showAdd ? "Close" : "Add"} onClick={onAdd} />
     </header>
   );
 };
