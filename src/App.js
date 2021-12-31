ee808474e6af5c3d1602517903ac8402baadea36
Alexander Amog <alex.amog@hotmail.com>
2021-12-31 02:33:22 -0800

    Added tasks as a global prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 import "./App.css";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Meta Interview",
+      date: "Dec 23, 2021",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Statistics Final",
+      date: "Dec 25, 2021",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Microsoft Technical Interview",
+      date: "Jan 4, 2022",
+      reminder: true,
+    },
+  ]);
   return (
     <div className="container">
-      <Header/>
-      <h1>Hello world</h1>
+      <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 };
