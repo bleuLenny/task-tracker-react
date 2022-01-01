6a8d9057eebc7c1a5943bfab2ec21dc556934c31
Alexander Amog <alex.amog@hotmail.com>
2021-12-31 16:19:28 -0800

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Tasks from "./components/Tasks";
 import "./App.css";
-import AddTask from "./components/AddTask";
-
+import Footer from "./components/Footer";
+import About from "./components/About";
+import TaskFinal from "./components/TaskFinal";
 const App = () => {
-  const [tasks, setTasks] = useState([
-  ]);
-
   const [showAddTask, setShowAddTask] = useState(false);
-
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
-
-  const toggleReminder = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
-      )
-    );
-  };
-
-  const addTask = (task) => {
-    const id = tasks.length + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
-  };
-
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        "No tasks available. Get started by adding a task with the Add button. "
-      )}
-    </div>
+    <Router>
+      <div className="container">
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        <Routes>
+          <Route path="/" exact element={<TaskFinal taskAdd={showAddTask} />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
