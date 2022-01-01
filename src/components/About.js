537cc08cbfcd0058b9c4ca99904c50cfb77ea323
Alexander Amog <alex.amog@hotmail.com>
2021-12-31 16:44:06 -0800

    Modified routes for deployment

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ const About = () => {
     return (
         <div>
         <h4>Version 1.0</h4>
-        <Link to='/task-tracker-react'>Go back</Link>            
+        <Link to='/task-tracker-react/'>Go back</Link>            
         </div>
     )
 }
